f3f08d55a61f11ea2dace18dd3c016ac3bfdda6c
Samyam Shrestha <samyamsth2@gmail.com>
2023-12-18 19:30:29 +0545

    First commit

---
+++ lab2a.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border=1 cellspacing=0 cellpadding=15>
+        <tr>
+            <th>Name</th>
+            <th>Marks</th>
+        </tr>
+
+        <?php
+    $students = array("Ram"=>30, "Hari"=>40, "Shyam"=>50,"Bari"=>60,"Aam"=>70);
+    $names=array_keys($students);
+    
+    for ($i=0; $i <count($students) ; $i++) { 
+        echo "<tr>
+        <td> $names[$i]</td> 
+        <td>".$students[$names[$i]]."</td>
+        </tr> ";
+    }
+    echo "</table>
+    Using foreach <br>";
+
+    echo"<table border=1 cellspacing=0 cellpadding=15>
+    <tr>
+        <th>Name</th>
+        <th>Marks</th>
+    </tr>";
+
+    foreach ($students as $name=>$marks) {
+        echo "<tr>
+        <td>".$name."</td>
+        <td>".$marks."</td>
+        </tr>";
+
+    }
+    
+    ?>
+    </table>
+</body>
+</html>
